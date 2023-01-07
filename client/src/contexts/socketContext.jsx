9ed20df8fdaa9c1a195375ab414635d199b6a4d7
Yugma Thakkar <yugma007.t@gmail.com>
2023-01-07 15:41:16 +0530

    delete everything, start anew

--- client/src/contexts/socketContext.jsx
+++
@@ -1,6 +0,0 @@
-
-// import { createContext } from "react";
-// import {io} from "socket.io-client"
-
-// export const socket = io.connect("http:localhost:3000/")
-// export const socketContext = createContext(null)
